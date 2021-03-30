00a68162ca568b6917815a24150b46242af574cb
Haziq <haziq.yusoff.aw@gmail.com>
2021-03-30 23:32:01 +0800

    pyvis visualize

--- code/interface.py
+++ code/interface.py
@@ -1,42 +1,68 @@
 import networkx as nx
 import pandas as pd
 import matplotlib.pyplot as plt
-from preprocessing import preprocess
+from preprocessing import preprocess, preprocess_create_graph
 import collections
 import pyvis
 from pyvis.network import Network
+import argparse
+
+# Get year
+parser = argparse.ArgumentParser()
+parser.add_argument("--year", type=int, default=2019)
+parser.add_argument("--analyze", default="Author")
+args = parser.parse_args()
+
+if args.analyze:
+    print('\nGenerating Graph \n')
+else:
+    raise ValueError("You must enter a parameter to analyze, --analyze Author/Rank/Position/Area")
 
 
-def cr8_graph():
-    net1 = Network(height='750px', width='100%')
+df = pd.read_csv('../data/SCSE_Records.csv')
+
+def get_netx(year):
     df = pd.read_csv('../data/SCSE_Records.csv')
-    df = preprocess(df,2019)
+    netx_graph = preprocess_create_graph(df, year)
+    return netx_graph
+
+def format_data(df):
+    # pyvis can only accept str or int types
+    df = preprocess(df, 2019)
     df['author-pid'] = df['author-pid'].astype(str)
     df['co-author-pid'] = df['co-author-pid'].astype(str)
     df = df.fillna(0)
     df['weight'] = df['weight'].astype(int)
+    print(df.info)
+    return df
 
-    sources = df['author-pid']
-    targets = df['co-author-pid']
-    weights = df['weight']
+def py_visualise(df, year, analyze_param, output_filename='graph.html',show_buttons=True,only_physics_buttons=True):
+    networkx_graph = get_netx(year)
+    format_data(df)
+    pyvis_graph = Network(height='750px', width='1000px')
 
-    edge_data = zip(sources, targets, weights)
+    # for each node and its attributes in the networkx graph
+    # attributes: {'author', 'year', 'Position', 'Gender', 'Management', 'Area', 'top_venue_count'}
+    for node, node_attrs in networkx_graph.nodes(data=True):
+        pyvis_graph.add_node(node, **node_attrs, label=node_attrs['author'])
+        print(node,node_attrs)
 
-    for e in edge_data:
-        src = e[0]
-        dst = e[1]
-        w = e[2]
 
-        net1.add_node(src, src, title=src)
-        net1.add_node(dst, dst, title=dst)
-        net1.add_edge(src, dst, value=w)
+    # for each edge and its attributes in the networkx graph
+    for source, target, edge_attrs in networkx_graph.edges(data=True):
+        # if value/width not specified directly, and weight is specified, set 'value' to 'weight'
+        if not 'value' in edge_attrs and not 'width' in edge_attrs and 'weight' in edge_attrs:
+            # place at key 'value' the weight of the edge
+            edge_attrs['value'] = edge_attrs['weight']
+        # add the edge
+        pyvis_graph.add_edge(source, target, **edge_attrs)
 
-    neighbor_map = net1.get_adj_list()
+    if show_buttons:
+        if only_physics_buttons:
+            pyvis_graph.show_buttons(filter_=['physics'])
+        else:
+            pyvis_graph.show_buttons()
 
-    for node in net1.nodes:
-        node['title'] += ' Neighbors:<br>' + '<br>'.join(neighbor_map[node['id']])
-        node['value'] = len(neighbor_map[node['id']])
+    return pyvis_graph.show(output_filename)
 
-    net1.show('gameofthrones.html')
-
-cr8_graph()
+py_visualise(df, year = args.year, output_filename='graph_output.html', analyze_param = args.analyze)
